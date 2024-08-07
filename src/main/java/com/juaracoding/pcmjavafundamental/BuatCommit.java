735050e07aca49339058ba7f75ff1ade0810202c
Pratasik Topah <pratasikgjmtopah@gmail.com>
2024-08-07 20:34:38 +0800

    Disini History Commit Nya.

---
+++ src/main/java/com/juaracoding/pcmjavafundamental/BuatCommit.java
@@ -0,0 +1,12 @@
+package com.juaracoding.pcmjavafundamental;/*
+IntelliJ IDEA 2024.1.4 (Ultimate Edition)
+Build #IU-241.18034.62, built on June 21, 2024
+@Author prata a.k.a. Pratasik Topah
+Java Developer
+Created on 07/08/2024 20:31
+@Last Modified 07/08/2024 20:31
+Version 1.0
+*/
+
+public class BuatCommit {
+}
